2ef513233e5a44a496c0eda53bdfd3e90ec53613
Rainer Jung <r.jung85@protonmail.com>
2023-08-12 02:45:43 +0200

    Spring Security without the WebSecurityConfigurerAdapter

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
