bce158b8d524ac603351a42df2652092c8a5eba2
Rainer Jung <r.jung85@protonmail.com>
2023-08-13 09:56:02 +0200

    use @Qualifier

--- src/main/java/com/example/demo/auth/ApplicationUserService.java
+++ src/main/java/com/example/demo/auth/ApplicationUserService.java
@@ -1,17 +1,18 @@
 package com.example.demo.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import static org.checkerframework.checker.nullness.Opt.orElseThrow;
-
 @Service
 public class ApplicationUserService implements UserDetailsService {
     private final ApplicationUserDao applicationUserDao;
 
-    public ApplicationUserService(ApplicationUserDao applicationUserDao) {
+    @Autowired
+    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
         this.applicationUserDao = applicationUserDao;
     }
 
